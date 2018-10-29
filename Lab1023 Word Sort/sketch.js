1fd6d94ccc2d7c7b04a7b010e3e1b5fc6f143c3e
Soren Victoria <791713@seq.org>
2018-10-29 08:27:08 -0700

    commit

---
+++ Lab1023 Word Sort/sketch.js
@@ -0,0 +1,15 @@
+//  Global variables
+var txt
+// put setup code here
+function setup() {
+  noCanvas();
+
+}//Soren Victoria
+function preload(){
+  // p5 has a function named "loadStrings"
+  txt = loadStrings("words.txt");
+}
+
+function setup() {
+
+}
